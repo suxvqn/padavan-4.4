cc095f9af497c5dee2cf7f8d5808a976e4803b80
666 <39879150+suxvqn@users.noreply.github.com>
2023-09-24 09:11:24 +0800

    更新 update_gfwlist.sh

--- trunk/user/shadowsocks/scripts/update_gfwlist.sh
+++ trunk/user/shadowsocks/scripts/update_gfwlist.sh
@@ -5,13 +5,14 @@ set -e -o pipefail
 #GFWLIST_URL="$(nvram get gfwlist_url)"
 logger -st "gfwlist" "Starting update..."
 curl -k -s -o /tmp/gfwlist_list_origin.conf --connect-timeout 15 --retry 5 https://gitlab.com/gfwlist/gfwlist/raw/master/gfwlist.txt
-lua /etc_ro/ss/gfwupdate.lua
+base64 -d /tmp/gfwlist_list_origin.conf > /tmp/gfwlist_list.conf
 count=`awk '{print NR}' /tmp/gfwlist_list.conf|tail -n1`
 if [ $count -gt 1000 ]; then
 rm -f /etc/storage/gfwlist/gfwlist_listnew.conf
 cp -r /tmp/gfwlist_list.conf /etc/storage/gfwlist/gfwlist_listnew.conf
-mtd_storage.sh save >/dev/null 2>&1
+##mtd_storage.sh save >/dev/null 2>&1
 mkdir -p /etc/storage/gfwlist/
+cp -rf /etc/storage/gfwlist/gfwlist_listnew.conf /etc/storage/gfwlist_list.conf
 logger -st "gfwlist" "Update done"
 if [ $(nvram get ss_enable) = 1 ]; then
 lua /etc_ro/ss/gfwcreate.lua
